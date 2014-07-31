9587a70036bf7005fc160b6cc5c5e2a4867d2932
Andrew Scherer <andrew@savvycontent.com>
2014-07-31 03:41:44 -0600

    Planning Phase
    Pre-code plan phase done.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-
-
-
-
-
-
-
+//Listen for a click event on a link/image
+$("#imageGallery a").click();
+  //1.1 Show/fade-in the overlay after a click
+  //1.2 Update overlay with the image linked from the link that was clicked on.
+  //1.3 Get child alt attrivute and set the caption with it.
+//2. Add overlay
+  //2.1 Add the image
+  //2.2 Add the caption
+//3. When overlay is clicked, hide and fadeout the overlay.
